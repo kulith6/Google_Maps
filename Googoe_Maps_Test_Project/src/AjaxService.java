f5a5417d0420a2e4b43e3f7f2baaf5afc772e272
kid <kulith1992@gmail.com>
2017-12-23 13:23:56 +0530

    Geocoding address find

--- Googoe_Maps_Test_Project/src/AjaxService.java
+++ Googoe_Maps_Test_Project/src/AjaxService.java
@@ -1,11 +1,7 @@
 
 
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.LinkedList;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,6 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 
 
 
@@ -39,8 +39,11 @@ public class AjaxService extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		Connection con;
+		PreparedStatement pst;
 		
-		System.out.println("hit servlet");
+		System.out.println("hit get");
 		
 		try {
 			
@@ -56,6 +59,18 @@ public class AjaxService extends HttpServlet {
 			// print object data
 			System.out.println(marker.getLat());
 		    
+			// connecting to mysql to store marker data
+			Class.forName("com.mysql.jdbc.Driver");
+			con = (Connection) DriverManager.getConnection ("jdbc:mysql://localhost/googlemaps", "root", "");
+			System.out.println("Successfully Connected to the Database");
+			pst = (PreparedStatement) con.prepareStatement("INSERT INTO marker (lat, lng, description) VALUES (?, ?, ?)");
+			pst.setDouble(1, marker.getLat());
+			pst.setDouble(2, marker.getLng());
+			pst.setString(3, marker.getDesc());
+			pst.executeUpdate();
+			System.out.println("Marker saved in database");
+			con.close();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -65,9 +80,44 @@ public class AjaxService extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		doGet(request, response);
-//	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		Connection con;
+		ArrayList<Marker> markerData = new ArrayList<Marker>();
+		
+		System.out.println("hit post");
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = (Connection) DriverManager.getConnection ("jdbc:mysql://localhost/googlemaps", "root", "");
+			System.out.println("Successfully Connected to the Database");
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("SELECT lat, lng, description FROM googlemaps.marker");
+			
+			while(rs.next()){
+				Marker marker = new Marker();
+				
+				double lat = rs.getDouble("lat");
+				double lng = rs.getDouble("lng");
+				String desc = rs.getString("description");
+				
+				marker.setLat(lat);
+				marker.setLng(lng);
+				marker.setDesc(desc);
+				
+				markerData.add(marker);
+			}
+			rs.close();
+			
+			String json = new Gson().toJson(markerData);
+            response.setContentType("application/json");
+            response.getWriter().write(json);
+            
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
 
 }
